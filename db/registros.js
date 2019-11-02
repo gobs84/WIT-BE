97f433f234d1a22a0e807fe4cc4fdc4a38377ff7
Veimar Choque <veimar.choque@truextend.com>
2019-11-02 14:17:00 -0400

    registerFooterApi: Created model of register format and connection to database

---
+++ db/registros.js
@@ -0,0 +1,28 @@
+const Joi = require('joi');
+const db = require('./conexion');
+ 
+const schema = Joi.object().keys({
+    email: Joi.string().email({ minDomainAtoms: 2 }),
+    intereses: Joi.array().items(Joi.string())
+});
+ 
+const registros = db.get('registros');
+ 
+function getAll() {
+    return registros.find();
+}
+ 
+function create(registro) {
+ 
+    const result = Joi.validate(registro, schema);
+    if (result.error == null) {
+        return registros.insert(registro);
+    } else {
+        return Promise.reject(result.error);
+    }
+}
+ 
+module.exports = {
+    create,
+    getAll
+};
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
  
 const app = express();
+
+const registros = require('./db/registros');
  
 app.use(morgan('tiny'));
 app.use(cors());
@@ -11,7 +13,23 @@ app.use(bodyParser.json());
  
 app.get('/', (req, res) => {
     res.json({
-        message: 'Behold The MEVN Stack!'
+        registro: 'Hello Moto!'
+    });
+});
+
+app.get('/registros', (req, res) => {
+    registros.getAll().then((registros) => {
+        res.json(registros);
+    });
+});
+
+app.post('/registros', (req, res) => {
+    console.log(req.body);
+    registros.create(req.body).then((message) => {
+        res.json(message);
+    }).catch((error) => {
+        res.status(500);
+        res.json(error);
     });
 });
  
